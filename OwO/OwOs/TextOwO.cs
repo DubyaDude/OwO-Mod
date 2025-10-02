948a15b77b93851b798c98968a9209e91c56550a
Teo45dore <sandrakelberlau@gmail.com>
2025-10-01 22:06:03 -0400

    Added IL2CPP/Mono cross-compatible OwOs

---
+++ OwO/OwOs/TextOwO.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine.UI;
+
+namespace OwO_Mod
+{
+    internal static class TextOwO
+    {
+        public static unsafe void Init()
+        {
+            OwO.owoHarmonyInstance.Patch(typeof(Text).GetMethod("set_text"), new HarmonyMethod(Utils.GetMethod(nameof(Patch))));
+        }
+
+        internal static void Patch(ref string value)
+        {
+            value = Utils.OwOify(value);
+        }
+    }
+}

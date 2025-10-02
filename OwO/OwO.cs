c80ba5ca6712bb76cb9ccb0b63acd14377ac322c
Teo45dore <sandrakelberlau@gmail.com>
2025-10-01 22:10:41 -0400

    Added updated csproj and upgraded OwO.cs

---
+++ OwO/OwO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using MelonLoader;
+
+namespace OwO_Mod
+{
+    public class OwO : MelonMod
+    {
+        public static HarmonyLib.Harmony owoHarmonyInstance;
+        public static MelonLogger.Instance owoLogger;
+
+        //Upgrade to new OnInitializeMelon because OnApplicationStart is depreciated.
+        public override void OnInitializeMelon()
+        {
+            owoHarmonyInstance = this.HarmonyInstance;
+            owoLogger = LoggerInstance;
+
+            try
+            {
+                TextOwO.Init();
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Warning("Error at Text: " + Environment.NewLine + e);
+            }
+            try
+            {
+                TextMeshOwO.Init();
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Warning("Error at TextMesh: " + Environment.NewLine + e);
+            }
+            try
+            {
+                TMPOwO.Init();
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Warning("Error at TextMeshPro: " + Environment.NewLine + e);
+            }
+        }
+
+        //Clear cache for more randomness and strings that are not likely to appear outside their own scene.
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            Utils.alreadyAppliedOwOs.Clear();
+        }
+    }
+}
+
+

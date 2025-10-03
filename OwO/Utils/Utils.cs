aeb8f6c9c3cd845221a4cb3f2a9b327e57d0eee3
DubyaDude <ushafiq141@gmail.com>
2025-10-02 20:19:43 -0400

    Fix compiler error

--- OwO/Utils/Utils.cs
+++ OwO/Utils/Utils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 
@@ -9,7 +10,7 @@ namespace OwO_Mod
         // Create a cache of already changed strings to prevent flickering of owoStrings when the text is getting set many times a second.
         // (And maybe improve performance??? It's probably negligible idk)
         public static Dictionary<string, string> alreadyAppliedOwOs = new Dictionary<string, string>();
-        
+
         public static MethodInfo GetMethod(string MethodName, BindingFlags bindingAttributes = BindingFlags.NonPublic | BindingFlags.Static)
         {
             StackTrace stackTrace = new StackTrace();
@@ -33,12 +34,13 @@ namespace OwO_Mod
             if (string.IsNullOrEmpty(text) || text.Contains("color="))
                 return text;
 
-            if (alreadyAppliedOwOs.ContainsKey(text) && alreadyAppliedOwOs[text] != null) {
-                return alreadyAppliedOwOs[text];
+            if (alreadyAppliedOwOs.TryGetValue(text, out var alreadyChanged))
+            {
+                return alreadyChanged;
             }
-            
+
             string oldText = text;
-            
+
             text = text.Replace('r', 'w').Replace('l', 'w').Replace('R', 'W').Replace('L', 'W');
 
             switch (rnd.Next(2))
@@ -77,9 +79,9 @@ namespace OwO_Mod
                     text += $" {owoStrings[rnd.Next(owoStrings.Length)]}";
                     break;
             }
-            
+
             alreadyAppliedOwOs.Add(oldText, text);
-            
+
             return text;
         }
     }
